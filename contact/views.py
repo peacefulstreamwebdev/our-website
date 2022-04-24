568e3c2538beb4fd4ba5e4221e52a8edfa7204ef
StoneMasons4106 <jake.d.rubach@gmail.com>
2022-04-24 12:28:51 -0400

    Base contact view in place with functioning links.

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def contact(request):
+    '''A view to return the home page'''
+
+    context = {
+        'page': 'contact',
+    }
+
+    return render(request, 'contact/contact.html', context)
