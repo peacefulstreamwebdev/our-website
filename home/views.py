89ad6f59632626794417d3e6d9a5023266093a04
StoneMasons4106 <jake.d.rubach@gmail.com>
2022-06-06 21:32:25 -0400

    Services now render on the homepage.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from contact.models import Content
+from services.models import Service
 
 # Create your views here.
 
@@ -7,10 +8,12 @@ def home(request):
     '''A view to return the home page'''
 
     contact = Content.objects.all()[0]
+    services = Service.objects.all()
 
     context = {
         'page': 'home',
         'contact': contact,
+        'services': services,
     }
 
     return render(request, 'home/home.html', context)
