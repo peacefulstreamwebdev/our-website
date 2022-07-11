9ff149035ee0bde2cb6e89205d3cdb84a3ad8872
StoneMasons4106 <jake.d.rubach@gmail.com>
2022-07-11 14:32:17 -0400

    Rendering tags, categories, counts to faq page.

--- faq/views.py
+++ faq/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Faq
+from .models import Faq, FaqCategory
+from taggit.models import Tag
 
 # Create your views here.
 
@@ -7,10 +8,19 @@ def faq(request):
     '''A view to return the FAQ page'''
 
     faqs = Faq.objects.all()
+    tags = Tag.objects.all()
+    categories = FaqCategory.objects.all()
+
+    lengths = []
+    for category in categories:
+        length = len(faqs.filter(category=category.id))
+        lengths.append(length)
 
     context = {
         'page': 'faq',
         'faqs': faqs,
+        'tags': tags,
+        'category_data': zip(categories, lengths),
     }
 
     return render(request, 'faq/faq.html', context)
