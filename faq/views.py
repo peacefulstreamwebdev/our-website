5a69e02d05300a8515ddf64d76a7da2a2f11de7e
StoneMasons4106 <jake.d.rubach@gmail.com>
2022-07-11 21:31:45 -0400

    Paginator for faq up and running.

--- faq/views.py
+++ faq/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from .models import Faq, FaqCategory
 from taggit.models import Tag
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -20,6 +21,7 @@ def faq(request):
     query = None
 
     if request.GET:
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             faqs = faqs.filter(category__name__in=categories)
@@ -38,12 +40,17 @@ def faq(request):
             queries = Q(question__icontains=query) | Q(answer__icontains=query)
             faqs = faqs.filter(queries)
 
+    paginator = Paginator(faqs, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
         'page': 'faq',
         'faqs': faqs,
         'tags': tags,
         'category_data': zip(categories, lengths),
         'search_term': query,
+        'page_obj': page_obj,
     }
 
     return render(request, 'faq/faq.html', context)
