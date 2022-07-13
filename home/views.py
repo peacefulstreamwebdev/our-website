79194f6a3fdf8968207a32aea9ecbd7c71ef33cf
StoneMasons4106 <jake.d.rubach@gmail.com>
2022-07-12 22:23:12 -0400

    Portfolio projects now rendering on home page as well.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from contact.models import Content
 from services.models import Service
+from portfolio.models import Project, ProjectCategory
 from .models import SlideContent
 
 # Create your views here.
@@ -11,12 +12,16 @@ def home(request):
     contact = Content.objects.all()[0]
     services = Service.objects.all()
     slides = SlideContent.objects.all()
+    projects = Project.objects.all()
+    categories = ProjectCategory.objects.all()
 
     context = {
         'page': 'home',
         'contact': contact,
         'services': services,
         'slides': slides,
+        'projects': projects,
+        'categories': categories,
     }
 
     return render(request, 'home/home.html', context)
